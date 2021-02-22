41b7998b2b1425e360453ad72133943fa33d322d
FilipePires17 <filipepiresec@gmail.com>
2021-02-22 15:24:34 -0400

    Generalização da Entidade Base

--- Classes/EntidadeBase.cs
+++ Classes/EntidadeBase.cs
@@ -1,7 +1,41 @@
+using System;
 namespace DIO.Series
 {
     public abstract class EntidadeBase
     {
         public int Id { get; protected set; }
+
+        protected Genero[] Generos = new Genero[10];
+		protected int countGeneros = 0;
+		protected string Titulo { get; set; }
+		protected string Sinopse { get; set; }
+		protected int Ano { get; set; }
+        protected bool Excluido {get; set;}
+
+        public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
+
+		public int retornaId()
+		{
+			return this.Id;
+		}
+        public bool retornaExcluido()
+		{
+			return this.Excluido;
+		}
+        public void Excluir() {
+            this.Excluido = true;
+        }
+
+		public void addGenero(Genero genero){
+			if(countGeneros<10){
+				Generos[countGeneros] = genero;
+				countGeneros++;
+			} else {
+				Console.WriteLine("Número máximo de gêneros atingido.");
+			}
+		}
     }
 }
--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -6,12 +6,6 @@ namespace DIO.Series
     public class Serie : EntidadeBase
     {
         // Atributos
-		private Genero[] Generos = new Genero[10];
-		private int countGeneros = 0;
-		private string Titulo { get; set; }
-		private string Sinopse { get; set; }
-		private int Ano { get; set; }
-        private bool Excluido {get; set;}
 
         // Métodos
 		public Serie(int id, string titulo, string sinopse, int ano)
@@ -40,30 +34,6 @@ namespace DIO.Series
 			return retorno;
 		}
 
-        public string retornaTitulo()
-		{
-			return this.Titulo;
-		}
-
-		public int retornaId()
-		{
-			return this.Id;
-		}
-        public bool retornaExcluido()
-		{
-			return this.Excluido;
-		}
-        public void Excluir() {
-            this.Excluido = true;
-        }
-
-		public void addGenero(Genero genero){
-			if(countGeneros<10){
-				Generos[countGeneros] = genero;
-				countGeneros++;
-			} else {
-				Console.WriteLine("Número máximo de gêneros atingido.");
-			}
-		}
+        
     }
 }
